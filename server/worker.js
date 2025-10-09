94e85eae631f11cd03a422dc8a2371162e41bafc
arbazz-siddique <arbazzsiddiquecse@gmail.com>
2025-10-09 09:39:52 +0530

    Implement working of quueue, use docker valkey.

---
+++ server/worker.js
@@ -0,0 +1,17 @@
+import { Worker } from "bullmq";
+
+const worker = new Worker(
+    'file-upload-queue',
+    async (job)=>{
+        console.log('Job', job.data)
+        const data = JSON.parse(job.data)
+        // path : data.path, read the pdf form path, chunk the pdf, call the openai embedding model for every chunk, store the chunk in qdrant db
+        
+    },
+    {concurrency:100, connection:{
+        host:'localhost',
+        port:'6379'
+    }}
+)
+
+

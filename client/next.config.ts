a1f914e73a5204756897cc2f826c65b95ed60151
arbazz-siddique <arbazzsiddiquecse@gmail.com>
2025-10-10 18:47:14 +0530

    added next-config

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   output: 'standalone', // or 'export' if you want static export
+  trailingSlash: true,
+  images: {
+    unoptimized: true // if you're having image issues
+  },
+  env: {
+    NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  }
 };
 
 export default nextConfig;

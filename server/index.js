3f8589d29863066fbcdb7722f95621456df23f01
arbazz-siddique <arbazzsiddiquecse@gmail.com>
2025-10-10 20:08:44 +0530

    fixed cors

--- server/index.js
+++ server/index.js
@@ -56,7 +56,8 @@ const corsOptions = {
   origin: [
     'https://docu-mind-scalable-rag-chatbot-with.vercel.app',
     'docu-mind-scalable-rag-cha-git-42f7d5-arbazz-siddiques-projects.vercel.app',
-'docu-mind-scalable-rag-chatbot-with-pdf-audio-upload-f0d8cvmkh.vercel.app',
+  'docu-mind-scalable-rag-chatbot-with-pdf-audio-upload-f0d8cvmkh.vercel.app',
+  'https://docu-mind-scalable-rag-chatbot-with-zeta.vercel.app',
     'http://localhost:3000'
   ],
   credentials: true,

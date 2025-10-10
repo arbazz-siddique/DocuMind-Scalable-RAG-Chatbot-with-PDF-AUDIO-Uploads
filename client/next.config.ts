ae73de1a7e170e442e05228b329779670c1d8147
arbazz-siddique <arbazzsiddiquecse@gmail.com>
2025-10-10 18:51:14 +0530

    fixing the error

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,15 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-   output: 'standalone', // or 'export' if you want static export
+  output: 'standalone',
   trailingSlash: true,
   images: {
-    unoptimized: true // if you're having image issues
-  },
-  env: {
-    NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    unoptimized: true
   }
 };
 
